afff85cc904e70a528c583a16925445f0b498d4f
Vladimir-Iluk <nezalejniy@ukr.net>
2025-07-01 23:40:37 +0300

    checking

--- SummerProj/Program.cs
+++ SummerProj/Program.cs
@@ -224,11 +224,8 @@ namespace SummerProj
             }
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
